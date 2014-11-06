b2f1673d7dea7430d98b86bc3cff2fcc11aa6353
sansaae <tdstamenov@gmail.com>
2014-11-06 17:54:20 +0200

    FrmNewStudent

---
+++ StudentSystemForInformationCRUD/StudentSystemForInformationCRUD/DB_Access.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace StudentSystemForInformationCRUD
+{
+    class DB_Access
+    {
+        SqlConnection conn = new SqlConnection("Data Source=SANSAAE-PC\\SQLEXPRESS;Initial Catalog=Students;Integrated Security=True");
+        
+        public void addstudent(string regNo, string Fname, string Lname, string phoneNo)
+        {
+            conn.Open();
+            // SqlCommand newCmd = new SqlCommand("insert into Students values = 'Fname', 'Lname', 'phoneNo' where regNo=@par1",conn);
+            SqlCommand newCmd = new SqlCommand("INSERT INTO Students ([RegNo], [Fname], [Lname], [Phone]) VALUES (@par1, @par2, @par3, @par4)", conn);
+            newCmd.Parameters.AddRange(new[] {
+                new SqlParameter("@par1", regNo),
+                new SqlParameter("@par2", Fname),
+                new SqlParameter("@par3", Lname),
+                new SqlParameter("@par4", phoneNo)
+            });
+            newCmd.ExecuteNonQuery();
+        }
+    }
+}
